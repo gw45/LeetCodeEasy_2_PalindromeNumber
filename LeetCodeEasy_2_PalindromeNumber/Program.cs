e22ad79de226f1f8c511aa0f043aed1d73be6c7a
gw45 <16625937+gw45@users.noreply.github.com>
2023-03-29 14:03:51 -0700

    Add project files.

---
+++ LeetCodeEasy_2_PalindromeNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine($" 123 palindrome? {IsPalindrome(123)}");
+Console.WriteLine($" 454 palindrome? {IsPalindrome(454)}");
+Console.WriteLine($" 765 palindrome? {IsPalindrome(765)}");
+
+bool IsPalindrome(int x)
+{
+    string numAsString = x.ToString();
+    char[] Characters = numAsString.ToCharArray();
+    Array.Reverse(Characters);
+    string revNumAsString = String.Join("", Characters);
+    return (numAsString == revNumAsString);
+}
+
+
